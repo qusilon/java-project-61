d6ab92e8c307fee60a27d6d379124e3eecf6001f
Alexandr Pershin <aleks98_06@mail.ru>
2025-01-21 21:40:43 +0700

    add even game and update READMI.md

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-### Hexlet tests and linter status:
+# The Brain Games
 [![Actions Status](https://github.com/qusilon/java-project-61/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/qusilon/java-project-61/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/2a22ea20be137f013737/maintainability)](https://codeclimate.com/github/qusilon/java-project-61/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/2a22ea20be137f013737/test_coverage)](https://codeclimate.com/github/qusilon/java-project-61/test_coverage)
+
+## Example of victory
+[![asciicast](https://asciinema.org/a/699186.svg)](https://asciinema.org/a/699186)
+
+## Example of defeat
+[![asciicast](https://asciinema.org/a/699189.svg)](https://asciinema.org/a/699189)
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,28 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greeting();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        int choice = scanner.nextInt();
+        System.out.println();
+        switch (choice) {
+            case 1:
+                Cli.greeting();
+                break;
+            case 2:
+                Even.playing();
+                break;
+            case 0:
+                break;
+        }
+        scanner.close();
     }
 }
